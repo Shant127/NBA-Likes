a9a57347a1740d6064c6cd65cd8986c64ac53a7e
Shant S <110202516+Shant127@users.noreply.github.com>
2022-09-20 10:22:14 -0700

    Add files via upload

---
+++ network.js
@@ -0,0 +1,19 @@
+var count = 0;
+
+function addCount(){
+    count++
+    var h4 = document.querySelector("#first")
+    h4.innerText = (count + " Like(s)")
+}
+var count2 = 0;
+function addCount2(){
+    count2++
+    var h4 = document.querySelector("#likes2")
+    h4.innerText = (count2 + " Like(s)")
+}
+var count3 = 0;
+function addCount3(){
+    count3++
+    var h4 = document.querySelector("#likes3")
+    h4.innerText = (count3 + " Like(s)")
+}
